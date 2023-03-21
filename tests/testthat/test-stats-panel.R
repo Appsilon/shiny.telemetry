90f252afa35e7dbd500867ccabc585216e4d8ec6
André Veríssimo <211358+averissimo@users.noreply.github.com>
2023-03-21 13:39:24 +0100

    update test with feedback from PR

--- tests/testthat/test-stats-panel.R
+++ tests/testthat/test-stats-panel.R
@@ -2,7 +2,7 @@ test_that("get_active_users", {
   log_data <- tibble::tibble(
     time = strptime("2023-03-20 15:00:00", format = "%Y-%m-%d %H:%M:%S") + c(10, 20, 30, 3601),
     username = c("a", "b", "a", "c"),
-    date = rep(Sys.Date(), 4)
+    date = Sys.Date()
   )
 
   result <- get_active_users(log_data)
@@ -29,25 +29,44 @@ test_that("get_actions_per_day", {
   )
 })
 
-# Change from tidyr::gather to tidyr::pivot_longer
-# for function: get_per_day_plot_data()
-test_that("gather pivot", {
-  dat <- tibble::tibble(
-    statistic = 1:10,
-    value = 11:20,
-    date = rep(Sys.Date(), 10)
+test_that("get_per_day_plot_data", {
+  date_initial <- Sys.Date()
+
+  base <- data.frame(date = date_initial)
+
+  per_day <- tibble::tribble(
+    ~date,                 ~users, ~sessions, ~time, ~actions,
+    date_initial + 0,      3,         4,     0,        5,
+    date_initial + 1,     31,        41,     8,       99
   )
 
-  gather_df <- dat %>%
-    tidyr::gather(key = "statistic", value = "value", -"date") %>%
-    dplyr::arrange(.data$date, .data$statistic, .data$value)
-
-  pivot_df <- dat %>%
-    tidyr::pivot_longer(c(-"date"), names_to = "statistic", values_to = "value") %>%
-    dplyr::arrange(.data$date, .data$statistic, .data$value)
+  per_day_plot_data <- tibble::tribble(
+               ~date,                 ~statistic, ~value, ~id,
+    date_initial + 0,    "logged users (unique)",      3,   3,
+    date_initial + 0,    "total opened sessions",      4,   1,
+    date_initial + 0, "avg session time (hours)",      0,   2,
+    date_initial + 0,  "total clicks and inputs",      5,   1,
+    #
+    date_initial + 1,    "logged users (unique)",     31,   3,
+    date_initial + 1,    "total opened sessions",     41,   1,
+    date_initial + 1, "avg session time (hours)",      8,   2,
+    date_initial + 1,  "total clicks and inputs",     99,   1,
+  ) %>%
+    dplyr::arrange(dplyr::across(tidyr::matches("[a-zA-Z]")))
 
   expect_equal(
-    pivot_df,
-    gather_df
+    get_per_day_plot_data(base, per_day) %>%
+      dplyr::arrange(dplyr::across(tidyr::matches("[a-zA-Z]"))),
+    per_day_plot_data %>% dplyr::filter(date == date_initial)
+  )
+
+  # 2 days
+  base2 <- dplyr::bind_rows(base, base + 1)
+
+  expect_equal(
+    get_per_day_plot_data(base2, per_day) %>%
+      dplyr::arrange(dplyr::across(tidyr::matches("[a-zA-Z]"))),
+    per_day_plot_data,
+    ignore_attr = TRUE
   )
 })
