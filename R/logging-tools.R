1e9cbdd5e887f593e3ba7766fb9a54c846ea036e
André Veríssimo <211358+averissimo@users.noreply.github.com>
2023-03-23 17:07:52 +0100

    browser_version supports moduleServers

--- R/logging-tools.R
+++ R/logging-tools.R
@@ -200,29 +200,42 @@ log_session_detail <- function(data_storage, detail) {
 #' shinyApp(ui = ui(), server = server)
 #' }
 #' @export
-browser_info_js <- shiny::tags$script(type = "text/javascript",
-  shiny::HTML("
-      $(document).on('shiny:sessioninitialized', function(event) {
-        var br_ver = (function(){
-          var ua= navigator.userAgent, tem, M;
-          M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\\/))\\/?\\s*(\\d+)/i) || [];
-          if (/trident/i.test(M[1])) {
-            tem = /\\brv[ :]+(\\d+)/g.exec(ua) || [];
-            return 'IE '+(tem[1] || '');
-          }
-          if (M[1]=== 'Chrome') {
-            tem= ua.match(/\\b(OPR|Edge)\\/(\\d+)/);
-            if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
-          }
-          M = M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-          if((tem= ua.match(/version\\/(\\d+)/i))!= null) M.splice(1, 1, tem[1]);
-          return M.join(' ');
-        })();
-        console.log('shiny session started', br_ver);
-        Shiny.setInputValue(\"app-browser_version\", br_ver);}
-      );"
+#' @examples
+#' browser_info_js("app")
+browser_info_js <- function(id = "") {
+  checkmate::expect_string(id, null.ok = TRUE)
+  shiny_namespace <- ""
+  if (id != "" && !is.null(id)) {
+    shiny_namespace <- glue::glue("{id}-")
+  }
+
+  shiny::tags$script(type = "text/javascript",
+    shiny::HTML(paste0(
+    "
+  $(document).on('shiny:sessioninitialized', function(event) {
+    var br_ver = (function(){
+      var ua= navigator.userAgent, tem, M;
+      M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\\/))\\/?\\s*(\\d+)/i) || [];
+      if (/trident/i.test(M[1])) {
+        tem = /\\brv[ :]+(\\d+)/g.exec(ua) || [];
+        return 'IE '+(tem[1] || '');
+      }
+      if (M[1]=== 'Chrome') {
+        tem= ua.match(/\\b(OPR|Edge)\\/(\\d+)/);
+        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+      }
+      M = M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+      if((tem= ua.match(/version\\/(\\d+)/i))!= null) M.splice(1, 1, tem[1]);
+      return M.join(' ');
+    })();
+    ",
+    glue::glue("Shiny.setInputValue(\"{shiny_namespace}browser_version\", br_ver);"),
+    "
+  });
+    "
+    ))
   )
-)
+}
 
 
 #' @rdname browser_info_js
