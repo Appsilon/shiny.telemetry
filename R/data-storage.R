bc7f6b9050549d438b0f2bed602f0dfddbdc8cbc
Recle E. Vibal <recle.vibal@appsilon.com>
2023-03-23 21:59:31 +0800

    Feat: Start adding R6 clss of log file

--- R/data-storage.R
+++ R/data-storage.R
@@ -313,3 +313,222 @@ DataStorageRSQLite <- R6::R6Class( # nolint object_name_linter
     }
   )
 )
+
+#' Data storage class for JSON Log File
+#'
+#' @description
+#' Implementation of the DataStorage R6 class to SQLite backend using a unified
+#' API for read/write operations
+#'
+#' @export
+#'
+#' @examples
+#' data_storage <- DataStorageLogFile$new(
+#'   username = "test_user",
+#'   log_file_path = tempfile(pattern = "user_stats", fileext = ".json")
+#' )
+#' data_storage$insert(list(id = "an_id", action = "click"))
+#' data_storage$insert(list(id = "another_id", action = "click"))
+#' data_storage$read_user_data(as.Date("2020-01-01"), as.Date("2025-01-01"))
+DataStorageLogFile <- R6::R6Class( # nolint object_name_linter
+  classname = "DataStorageRSQLite",
+  inherit = DataStorage,
+  #
+  # Public
+  public = list(
+    # @title initialize RSQLite storage provider
+    # @param username string with username of the current session
+    #
+
+    #' @description
+    #' Initialize the data storage class
+    #' @param username string with username of the current session
+    #' @param session_id string with custom session id (should not be used)
+    #' @param log_file_path string with path to JSON log file
+
+    initialize = function(
+    username, session_id = NULL, log_file_path = "user_logs.json"
+    ) {
+      super$initialize(username, session_id)
+      logger::log_info("path to file: {log_file_path}")
+      log_threshold(INFO)
+      log_formatter(formatter_json)
+      log_layout(layout_json(fields = c("time", "msg")))
+      log_appender(appender_file(log_file_path))
+    },
+
+    #' @description Insert new data
+    #' @param values list of values to write to database
+    #' @param bucket name of table to write
+    #' @param add_username boolean flag that indicates if line should include
+    #' the username of the current session
+
+    insert = function(values, bucket = "user_log", add_username = TRUE) {
+      values <- private$insert_checks(values, bucket, add_username)
+
+      private$write(values, bucket)
+    },
+
+    #' @description read all user data from SQLite
+    #' @param date_from date representing the starting day of results
+    #' @param date_to date representing the last day of results
+
+    read_user_data = function(date_from, date_to) {
+      db_data <- private$read_data("user_log", date_from, date_to)
+
+      if (NROW(db_data) > 0) {
+        return(dplyr::mutate(db_data, date = as.Date(.data$time)))
+      }
+      db_data
+    },
+
+    #' @description read all session data from SQLite
+    #' @param date_from date representing the starting day of results
+    #' @param date_to date representing the last day of results
+
+    read_session_data = function(date_from, date_to) {
+      db_data <- private$read_data("session_details", date_from, date_to)
+
+      db_data %>%
+        dplyr::select("session", "detail") %>%
+        dplyr::group_by(.data$session) %>%
+        dplyr::summarise(title = paste(.data$detail, collapse = " | "))
+    },
+
+    #' @description read all session data
+
+    close = function() {
+      private$close_connection()
+    }
+  ),
+  #
+  # Private
+  private = list(
+    # Private Fields
+    db_con = NULL,
+    #
+    #
+    # Private methods
+
+    # @name connect
+    # Makes connection to database based on passed config data
+    # @param db_path string with path to file
+
+    connect = function(db_path) {
+      # Initialize connection with sqlite database
+      private$db_con <- odbc::dbConnect(RSQLite::SQLite(), dbname = db_path)
+    },
+
+    # @description disconnect with sql database
+
+    close_connection = function() {
+      odbc::dbDisconnect(private$db_con)
+    },
+    insert_checks = function(values, bucket, add_username) {
+      checkmate::expect_string(bucket)
+      checkmate::expect_list(values)
+
+      if ("time" %in% names(values)) {
+        rlang::abort(paste0(
+          "You must not pass 'time' value into database.",
+          " It is set automatically."
+        ))
+      }
+
+      if ("session" %in% names(values)) {
+        rlang::abort(paste0(
+          "You must not pass 'session' value into database.",
+          " It is set automatically."
+        ))
+      }
+
+      if ("username" %in% names(values)) {
+        rlang::abort(paste0(
+          "You must not pass 'username' value into database.",
+          " It is set automatically."
+        ))
+      }
+
+      values$time <- as.character(Sys.time())
+      values$session <- private$.session_id
+
+      if (isTRUE(add_username)) {
+        values$username <- private$.username
+      }
+
+      values
+    },
+
+    initialize_connection = function(username, table_schemes) {
+      table_schemes <- list(
+        user_log = c(
+          time = "TIMESTAMP",
+          session = "TEXT",
+          username = "TEXT",
+          action = "TEXT",
+          id = "TEXT",
+          value = "TEXT"
+        ),
+        session_details = c(
+          time = "TIMESTAMP", session = "TEXT", detail = "TEXT"
+        )
+      )
+
+      table_names <- names(table_schemes)
+
+      purrr::walk2(
+        table_names, table_schemes, private$create_table_from_schema
+      )
+      NULL
+    },
+    #
+    create_table_from_schema = function(table_name, table_scheme) {
+      if (!(table_name %in% odbc::dbListTables(private$db_con))) {
+        logger::log_debug("Creating table {table_name}")
+        create_table_query <- odbc::sqlCreateTable(
+          con = private$db_con,
+          table = table_name,
+          fields = table_scheme,
+          row.names = FALSE
+        )
+        res <- odbc::dbSendQuery(conn = private$db_con, create_table_query)
+        odbc::dbClearResult(res)
+      }
+      NULL
+    },
+    #
+    #
+    write = function(values, bucket) {
+      checkmate::expect_string(bucket)
+      checkmate::expect_list(values)
+
+      send_query_df <- as.data.frame(values, stringsAsFactors = FALSE)
+
+      odbc::dbWriteTable(
+        private$db_con,
+        bucket,
+        send_query_df,
+        overwrite = FALSE,
+        append = TRUE,
+        row.names = FALSE
+      )
+    },
+    #
+    #
+    read_data = function(bucket, date_from, date_to) {
+      checkmate::expect_string(bucket)
+      checkmate::expect_date(date_from)
+      checkmate::expect_date(date_to)
+
+      query <- glue::glue(
+        .sep = " ",
+        "SELECT *",
+        "FROM {bucket}",
+        "WHERE date(time) >= '{date_from}' AND date(time) <= '{date_to}'"
+      )
+
+      odbc::dbGetQuery(private$db_con, query) %>%
+        dplyr::tibble()
+    }
+  )
+)
