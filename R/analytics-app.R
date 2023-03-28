c91f54affcd2b54c7e13240b210c3a385009a05f
Recle E. Vibal <recle.vibal@appsilon.com>
2023-03-28 13:45:49 +0800

    Feat: Creates function to run example dashboard

---
+++ R/analytics-app.R
@@ -0,0 +1,9 @@
+#' Run example telemetry analytics dashboard
+#'
+#' @param data_storage data_storage instance that will handle all backend read
+#' and writes.
+#'
+#' @export
+run_analytics_dashboard <- function(data_storage) {
+  shiny::shinyAppDir(appDir = system.file("examples/app/analytics", package = "shiny.telemetry"))
+}
