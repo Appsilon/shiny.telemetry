8099b6315f7b8edfe9435da0efdcc8bf936b2751
André Veríssimo <211358+averissimo@users.noreply.github.com>
2023-03-28 15:22:08 +0200

    minor change to test path in rsconnect

--- plumber_rest_api/entrypoint.R
+++ plumber_rest_api/entrypoint.R
@@ -1,3 +1,7 @@
+logger::log_info("path here: {fs::path_abs(here::here())}")
+logger::log_info("path getwd: {fs::path_abs(getwd())}")
+logger::log_info("files here: {paste(list.files(getwd()), collapse = \", \")}")
+
 plumber::plumb(
   file = "app/main.R"
 )
