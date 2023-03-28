6d26ea85db6a40f192e758134b588c20b5afbcc1
Recle E. Vibal <recle.vibal@appsilon.com>
2023-03-28 20:44:35 +0800

    Feat: Use default viewer for analytics dashboard

--- inst/examples/app/analytics.R
+++ inst/examples/app/analytics.R
@@ -5,16 +5,8 @@ library(RSQLite)
 # install.packages("shiny.telemetry", dependencies = TRUE)
 library(shiny.telemetry)
 
-# define function hot to get username
-get_user <- function(session) {
-  username <- shiny::isolate(shiny::parseQueryString(session$clientData$url_search)$username)
-  if (is.null(username)) username <- "unknownUser"
-  shiny::req(username)
-  return(username)
-}
-
 data_storage <- DataStorageRSQLite$new(
-  username = get_user(session = NULL), db_path = "user_stats.sqlite"
+  username = "viewer", db_path = "user_stats.sqlite"
 )
 
 run_analytics_dashboard(data_storage)
