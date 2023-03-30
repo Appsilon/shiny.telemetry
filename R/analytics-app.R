3da21d88852d5f43feab31629f10de8f03ccfcef
Recle E. Vibal <recle.vibal@appsilon.com>
2023-03-30 13:21:59 +0800

    Feat: Uses options and getOption instead
    shinyOptions and getShinyOption are not global in scope,
    so trying option and getOption instead.

--- R/analytics-app.R
+++ R/analytics-app.R
@@ -5,6 +5,6 @@
 #'
 #' @export
 run_analytics_dashboard <- function(data_storage) {
-  shiny::shinyOptions(data_storage = data_storage)
+  options(data_storage = data_storage)
   shiny::shinyAppDir(appDir = system.file("templates/app/analytics", package = "shiny.telemetry"))
 }
