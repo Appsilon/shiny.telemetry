84053cb7d87bc28df7fd8632d6999b74936f0c79
André Veríssimo <211358+averissimo@users.noreply.github.com>
2023-09-12 18:56:17 +0200

    feat: expand the tests to SQLite

--- tests/testthat/test-data_storage-sqlite.R
+++ tests/testthat/test-data_storage-sqlite.R
@@ -6,3 +6,29 @@ test_that("[SQLite] DataStorage should be able to insert and read", {
 
   test_common_data_storage(data_storage)
 })
+
+test_that("[SQLite] DataStorage should be able to insert and read events without details", {
+  db_path <- tempfile(fileext = ".sqlite")
+  withr::defer(file.remove(db_path))
+
+  data_storage <- DataStorageSQLite$new(db_path = db_path)
+  test_common_empty_details(data_storage)
+})
+
+test_that("[SQLite] Insert and read custom fields with length > 1", {
+  db_path <- tempfile(fileext = ".sqlite")
+  withr::defer(file.remove(db_path))
+
+  data_storage <- DataStorageSQLite$new(db_path = db_path)
+
+  test_common_len_gt_1(data_storage)
+})
+
+test_that("[SQLite] Insert and read custom fields with length > 1 on a pre-populated file", {
+  db_path <- tempfile(fileext = ".sqlite")
+  withr::defer(file.remove(db_path))
+
+  data_storage <- DataStorageSQLite$new(db_path = db_path)
+
+  test_common_len_gt_1_alt(data_storage)
+})
