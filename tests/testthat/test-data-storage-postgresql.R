33e39ce1420d049571c6a07423b04e966435843c
arunkodati77 <arunkodati77@gmail.com>
2024-03-11 19:55:22 -0400

    Updated PostgreSQL test case
    Updated PostgreSQL test case such that it will cover test using both postgres drivers (RPostgres, RPostgreSQL)

--- tests/testthat/test-data-storage-postgresql.R
+++ tests/testthat/test-data-storage-postgresql.R
@@ -1,3 +1,11 @@
 
 # Test suite common to data storages (see `helper-data_storage.R`)
-test_that_common_data_storage(init_test_postgres, "PostgreSQL")
+withr::with_envvar(
+  c("TEST_POSTGRESQL_DRIVER" = "RPostgres"),
+  code = test_that_common_data_storage(init_test_postgres, "PostgreSQL")
+)
+
+withr::with_envvar(
+  c("TEST_POSTGRESQL_DRIVER" = "RPostgreSQL"),
+  code = test_that_common_data_storage(init_test_postgres, "PostgreSQL")
+)
