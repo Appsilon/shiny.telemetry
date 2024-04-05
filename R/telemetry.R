50aa27cfa818fc6772d419c1bb7f3e027a50eb8a
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-04-05 10:34:16 +0200

    fix: use anonymous function instead of formula

--- R/telemetry.R
+++ R/telemetry.R
@@ -607,7 +607,7 @@ Telemetry <- R6::R6Class( # nolint object_name_linter
             excluded_inputs_regex <- excluded_inputs_regex %>%
               purrr::map_chr(trimws) %>%
               purrr::keep(~ nzchar(.x))  %>%
-              purrr::map_chr(~ stringr::str_escape(.x)) %>%
+              purrr::map_chr(stringr::str_escape) %>%
               paste(collapse = "|") %>%
               sub(pattern = "\\|$", replacement = "")
             filtered_names <- setdiff(
