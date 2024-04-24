9d39ff683020ae383e128c2933b6c7693835cd82
Wahiduzzaman Khan <wahiduzzamankhan007@gmail.com>
2024-04-24 17:56:34 +0600

    fix: fix the path to LogFile in analytics app

--- inst/examples/app/analytics/app.R
+++ inst/examples/app/analytics/app.R
@@ -17,8 +17,11 @@ library(DT)
 library(shiny.telemetry)
 
 # Default storage backend using LogFile
+log_file_path <- file.path("../instrumentation/user_stats.txt")
+if (!dir.exists(dirname(log_file_path))) log_file_path <- "user_stats.txt"
+
 data_storage <- DataStorageLogFile$new(
-  log_file_path = file.path("../instrumentation/user_stats.txt")
+  log_file_path = log_file_path
 )
 
 # This sample application includes a configuration for RSConnect deployments,
